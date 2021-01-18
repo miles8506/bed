87ecff567f8806a024abcc3825f527bc09558119
miles8506 <74091099+miles8506@users.noreply.github.com>
2021-01-18 11:36:22 +0800

    ok

---
+++ js/index/index.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+    // moblenav區塊
+    const navicon = document.querySelector('.navicon');
+    const nav_list = document.querySelector('.nav_list');
+    let flag1 = 0;
+    navicon.addEventListener('click', () => {
+        if (flag1 === 0) {
+            nav_list.style.display = 'block';
+            document.body.style.backgroundColor = 'rgba(0,0,0, .3)';
+            flag1++;
+        } else if (flag1 === 1) {
+            nav_list.style.display = 'none';
+            document.body.style.backgroundColor = '';
+            flag1--;
+        };
+    });
+    const tophigh = document.querySelector('.contain-row2');
+    // const topicon = document.querySelector('.topicon')
+    document.addEventListener('scroll', () => {
+        if (window.pageYOffset > tophigh.offsetTop) {
+            $('.topicon').fadeIn(500);
+        } else {
+            $('.topicon').fadeOut(500);
+        }
+    })
+});
